e7f4a3f4a0ae345e40ebeabcf4ee87b62c3bcf9d
Maurizio <vorrin.tal@gmail.com>
2014-05-30 11:39:32 +0100

    Customer scrollbar buttons added

---
+++ Assets/scripts/ScrollviewScrollButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollviewScrollButton : MonoBehaviour {
+    public UIScrollView scrollView;
+    public enum Directions
+    {
+        Right,
+        Left
+    }
+    public Directions direction;
+    public float amountToScroll = 30f;
+
+
+    public void OnClick()
+    {
+        float delta = 0f; ;
+        switch (direction){
+            case Directions.Right:
+                delta = amountToScroll;
+                break;
+            case Directions.Left:
+                delta = -amountToScroll;
+                break;
+        }
+        scrollView.Scroll(delta);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
